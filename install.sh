b9019146efb64f5c5235b007f821dd4baa03af22
Markus Weigelt <markus.weigelt@slub-dresden.de>
2024-06-05 09:13:28 +0200

    Rename three to threejs and improve install file

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-install_module() {
+DLF_3DHOP_VERSION="4.3"
+DLF_ATON_VERSION="3.0.10"
+DLF_MODEL_VIEWER_VERSION="3.5.0"
+DLF_THREEJS_VERSION="r165"
+
+install_zip_module() {
   local MODULE_DIR_PATH="./$2/modules"
   rm -rf $MODULE_DIR_PATH
   local MODULE_FILE_PATH="$MODULE_DIR_PATH/module.zip"
@@ -10,9 +15,11 @@ install_module() {
   rm $MODULE_FILE_PATH
 }
 
-install_module "https://www.3dhop.net/download/3DHOP_4.3.zip" "dlf-3dhop"
-install_module "https://github.com/mrdoob/three.js/archive/refs/tags/r165.zip" "dlf-three"
-install_module "https://github.com/phoenixbf/aton/archive/refs/tags/3.0.10.zip" "dlf-aton"
+install_zip_module "https://www.3dhop.net/download/3DHOP_$DLF_3DHOP_VERSION.zip" "dlf-3dhop"
+install_zip_module "https://github.com/mrdoob/three.js/archive/refs/tags/$DLF_THREEJS_VERSION.zip" "dlf-threejs"
+install_zip_module "https://github.com/phoenixbf/aton/archive/refs/tags/$DLF_ATON_VERSION.zip" "dlf-aton"
 
-mkdir -p "dlf-model-viewer/modules/model-viewer/3.5.0/"
-wget "https://ajax.googleapis.com/ajax/libs/model-viewer/3.5.0/model-viewer.min.js" -O "dlf-model-viewer/modules/model-viewer/3.5.0/model-viewer.min.js"
+DLF_MODEL_VIEWER_MODEL_PATH="./dlf-model-viewer/modules/model-viewer"
+rm -rf $DLF_MODEL_VIEWER_MODEL_PATH
+mkdir -p "./$DLF_MODEL_VIEWER_MODEL_PATH/$DLF_MODEL_VIEWER_VERSION"
+wget "https://ajax.googleapis.com/ajax/libs/model-viewer/$DLF_MODEL_VIEWER_VERSION/model-viewer.min.js" -O "$DLF_MODEL_VIEWER_MODEL_PATH/$DLF_MODEL_VIEWER_VERSION/model-viewer.min.js"

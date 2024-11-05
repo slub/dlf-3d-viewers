97dc4fc5195a56f57add87f31ed3c97f90efff56
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2024-11-05 16:51:53 +0100

    Remove submodule (#87)
    * add thedworal dfg-3dviewer
    
    * Improve css
    
    * Remove submodule, update versions

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 DLF_3DHOP_VERSION="4.3"
 DLF_ATON_VERSION="3.0.10"
 DLF_KOMPAKKT_STANDALONE_VERSION="main"
-DLF_MODEL_VIEWER_VERSION="3.5.0"
-DLF_THREEJS_VERSION="r165"
+DLF_MODEL_VIEWER_VERSION="4.0.0"
+DLF_THEDWORAK_DFG_3DVIEWER_VERSION="main"
+DLF_THREEJS_VERSION="r170"
 
 install_zip_module() {
   local MODULES_DIR_PATH="./$2/modules"
@@ -27,6 +28,7 @@ install_lib_module() {
 install_zip_module "https://www.3dhop.net/download/3DHOP_$DLF_3DHOP_VERSION.zip" "dlf-3dhop"
 install_zip_module "https://github.com/mrdoob/three.js/archive/refs/tags/$DLF_THREEJS_VERSION.zip" "dlf-threejs"
 install_zip_module "https://github.com/phoenixbf/aton/archive/refs/tags/$DLF_ATON_VERSION.zip" "dlf-aton"
+install_zip_module "https://github.com/thedworak/dfg_3dviewer/archive/refs/heads/$DLF_THEDWORAK_DFG_3DVIEWER_VERSION.zip" "dlf-thedworak-dfg-3dviewer"
 
 install_lib_module "https://ajax.googleapis.com/ajax/libs/model-viewer/$DLF_MODEL_VIEWER_VERSION/model-viewer.min.js" "dlf-model-viewer" "model-viewer/$DLF_MODEL_VIEWER_VERSION"
 install_lib_module "https://raw.githubusercontent.com/Kompakkt/StandaloneViewer/$DLF_KOMPAKKT_STANDALONE_VERSION/kompakkt-standalone.min.js" "dlf-kompakkt" "kompakkt-standalone/$DLF_KOMPAKKT_STANDALONE_VERSION"

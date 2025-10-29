c70ee432779c3da92f53ca0fd3ef99247dba1f8f
Markus Weigelt <markus.weigelt@slub-dresden.de>
2025-10-29 10:33:50 +0100

    Ĉhange aim viewer to a legacy version

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ DLF_3DHOP_VERSION="4.3"
 DLF_ATON_VERSION="3.0.10"
 DLF_KOMPAKKT_STANDALONE_VERSION="main"
 DLF_MODEL_VIEWER_VERSION="4.0.0"
-DLF_THEDWORAK_DFG_3DVIEWER_VERSION="npm-refactor"
+#DLF_THEDWORAK_DFG_3DVIEWER_VERSION="npm-refactor"
 DLF_THREEJS_VERSION="r170"
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
@@ -33,6 +33,8 @@ install_zip_module "https://github.com/mrdoob/three.js/archive/refs/tags/$DLF_TH
 install_zip_module "https://github.com/mrdoob/three.js/archive/refs/tags/$DLF_THREEJS_VERSION.zip" "dlf-jena-3dviewer-vrglasses"
 install_zip_module "https://github.com/mrdoob/three.js/archive/refs/tags/$DLF_THREEJS_VERSION.zip" "dlf-threejs"
 install_zip_module "https://github.com/phoenixbf/aton/archive/refs/tags/$DLF_ATON_VERSION.zip" "dlf-aton"
-install_zip_module "https://github.com/thedworak/dfg_3dviewer/archive/refs/heads/$DLF_THEDWORAK_DFG_3DVIEWER_VERSION.zip" "dlf-aim-3d"
+#install_zip_module "https://github.com/thedworak/dfg_3dviewer/archive/refs/heads/$DLF_THEDWORAK_DFG_3DVIEWER_VERSION.zip" "dlf-aim-3d"
+#install legacy state
+install_zip_module "https://github.com/thedworak/dfg_3dviewer/archive/37571554340792b2b6b52052921251f3c2e0ae8a.zip" "dlf-aim-3d"
 install_lib_module "https://ajax.googleapis.com/ajax/libs/model-viewer/$DLF_MODEL_VIEWER_VERSION/model-viewer.min.js" "dlf-model-viewer" "model-viewer/$DLF_MODEL_VIEWER_VERSION"
 install_lib_module "https://raw.githubusercontent.com/Kompakkt/StandaloneViewer/$DLF_KOMPAKKT_STANDALONE_VERSION/kompakkt-standalone.min.js" "dlf-kompakkt" "kompakkt-standalone/$DLF_KOMPAKKT_STANDALONE_VERSION"

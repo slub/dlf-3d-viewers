f1f9ca271188bfd5f7316b0fd812d8b594da55d2
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2025-12-08 13:27:12 +0100

    Update dfg_3dviewer download link in install.sh

--- install.sh
+++ install.sh
@@ -35,6 +35,6 @@ install_lib_module() {
 #install_zip_module "https://github.com/phoenixbf/aton/archive/refs/tags/$DLF_ATON_VERSION.zip" "dlf-aton"
 #install_zip_module "https://github.com/thedworak/dfg_3dviewer/archive/refs/heads/$DLF_THEDWORAK_DFG_3DVIEWER_VERSION.zip" "dlf-aim-3d"
 #install legacy state
-install_zip_module "https://github.com/thedworak/dfg_3dviewer/raw/a5ef1069c30e5281dc1cddf774c087f2f6074cce/dfg_3dviewer-dist.zip" "dlf-aim-3d"
+install_zip_module "https://github.com/thedworak/dfg_3dviewer/raw/cdf98083e791d9232c9908f107aec3ad4441c7b1/dfg_3dviewer-dist.zip" "dlf-aim-3d"
 #install_lib_module "https://ajax.googleapis.com/ajax/libs/model-viewer/$DLF_MODEL_VIEWER_VERSION/model-viewer.min.js" "dlf-model-viewer" "model-viewer/$DLF_MODEL_VIEWER_VERSION"
 #install_lib_module "https://raw.githubusercontent.com/Kompakkt/StandaloneViewer/$DLF_KOMPAKKT_STANDALONE_VERSION/kompakkt-standalone.min.js" "dlf-kompakkt" "kompakkt-standalone/$DLF_KOMPAKKT_STANDALONE_VERSION"
